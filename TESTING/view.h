1497689b0b44be113a623ca900ebd492230abaa7
Monish <monish.ramadoss@outlook.com>
2022-11-16 05:22:23 -0500

    refactor

---
+++ TESTING/view.h
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+
+
+struct offset
+{
+	size_t upper;
+	size_t lower;
+};
+
+
+class view final
+{
+	uint32_t n_dims_;
+	char data_size_{};
+
+	uint32_t* shape_;
+	size_t* size_;
+	size_t* stride_;
+
+	uint32_t n_offsets_;
+	offset** offset_;
+public:
+	view(const uint32_t* shape, uint32_t dims, char data_size);
+	view(uint32_t* shape, uint32_t dims, char data_size);
+
+	~view();
+	view(const view&v);
+	view(view&& v) noexcept;
+	view& operator=(view&& v) noexcept;
+	[[nodiscard]] uint32_t ndims() const;
+	[[nodiscard]] size_t size(uint32_t idx = 0) const;
+	[[nodiscard]] uint32_t shape(uint32_t idx = 0) const;
+	[[nodiscard]] size_t bytes_length() const;
+	[[nodiscard]] size_t count(uint32_t start_axis = 0) const;
+	[[nodiscard]] size_t count(const uint32_t start_axis, uint32_t end_axis=-1) const;
+	view index(uint32_t idx, int dim);
+	view reshape(const uint32_t* shape, uint32_t dims);
+
+};
---
+++ prototype.cpp
@@ -0,0 +1,113 @@
+#include "vulkan_dnn.h"
+#include <chrono>
+using namespace std::chrono;
+
+using namespace std;
+struct temp_param
+{
+	int size;
+};
+
+int main()
+{
+	init();
+
+
+	/*
+	tensor r1 = arange_t<float>(2048);
+	tensor r2 = zeros<float>({ 2048 });
+	reduce_sum(r1, r2);
+	r2.sync(false);
+
+	*/
+
+
+	//tensor t1 = arange_t<float>(64);
+	//t1.reshape({ 8, 8 });
+	//tensor t2 = arange_t<float>(64);
+	//t2.reshape({ 8, 8 });
+	//tensor t3({ 8, 8 });
+	//matmul(t1, t2, t3);
+	//t3.sync(false);
+
+
+	//// 16384 
+	//uint32_t m = 16384;
+	//uint32_t n = 8192;
+	//uint32_t k = 8192; 
+	//m = 4096;
+	//n = 4096;
+	//k = 4096;
+	//tensor t4 = tensor::ones<float>({ m, n });
+	//tensor t5 = tensor::ones<float>({ n, k });
+	//tensor t6 = tensor::zeros<float>({ m, k });
+
+	//matmul(t4, t5, t6);
+	//t6.sync(false);
+
+	//const auto* f1 = static_cast<float*>(t3.get_host_data()->ptr);
+	//const auto* f2 = static_cast<float*>(t6.get_host_data()->ptr);
+	//const auto* f3 = static_cast<float*>(r2.get_host_data()->ptr);
+
+
+
+
+	/*for(i = 0; i < t6.get_size(); ++i)
+	{
+		if (f2[i] != 4096)
+			break;
+	}
+	bool val = i  == t6.get_size();
+	std::cout << val << '\n';
+	for (i = 0; i < 8; ++i)
+	{
+		for(size_t j = 0; j < 8; ++j)
+		{
+			std::cout << f1[i * 8 + j] << " ";
+		}
+		std::cout << '\n';
+	}*/
+
+	/*float acc = 0;
+	for(uint32_t k = 0; k < 2048; ++k)
+	{
+		if(f3[k] != 0)
+			std::cout << k << " " << f3[k] << '\n';
+		acc += f3[k];
+	}
+	std::cout << acc << '\n';
+	*/
+
+	//CUDA :	60-80 microseconds;
+	//VK :		1268 microseconds
+
+	tensor inpt = arange_t(0.0f, 18.0f, 1.0f);
+	inpt.reshape({ 1, 2, 3, 3 });
+	//tensor::ones<float>({ 2,2,3,3 });
+
+	std::vector<uint32_t> params = {
+		2, 2,		// kernel_size
+		1, 1,		// stride
+		0, 0,		// padding
+		1, 1,		// dilation	
+		3, 3,	// input shape
+		2, 2, 2, 2		// output shape
+	};
+	tensor oupt = tensor::zeros<float>({ 2, 4, 4 });
+	auto param_tensor = tensor({ static_cast<uint32_t>(params.size()) }, UINT);
+	auto* blk = param_tensor.get_host_data();
+	memcpy(blk->ptr, params.data(), sizeof uint32_t * params.size());
+	param_tensor.sync();
+
+	unfold<2>(inpt, oupt, param_tensor);
+	oupt.sync(false);
+	inpt.sync(false);
+
+	const auto* col = static_cast<float*>(oupt.get_host_data()->ptr);
+	for (size_t i = 0; i < oupt.get_size(); ++i)
+	{
+		std::cout << col[i] << " ";
+	}
+
+	return 0;
+}
